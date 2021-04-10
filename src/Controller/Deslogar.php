2b1ae9ebe0b3d3f7a3fe48aa93721bd1392b2987
Carlos Accyole <carlos_accyole@hotmail.com>
2021-04-09 23:45:44 -0300

    Adicionando o Response

--- src/Controller/Deslogar.php
+++ src/Controller/Deslogar.php
@@ -2,6 +2,7 @@
 
 namespace Alura\Cursos\Controller;
 
+use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -12,6 +13,6 @@ class Deslogar implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         session_destroy();
-        header('Location: /login');
+        return new Response(302, ['Location' => '/login']);
     }
 }
